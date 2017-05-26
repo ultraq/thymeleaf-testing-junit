a301486e14c6ea3c8160e124b9af1dad75e50bab
Emanuel Rabina <emanuelrabina@gmail.com>
2017-05-26 16:10:35 +1200

    Use @Delegate, save heaps of lines of code, #5

--- Source/nz/net/ultraq/thymeleaf/testing/JUnitTestReporter.groovy
+++ Source/nz/net/ultraq/thymeleaf/testing/JUnitTestReporter.groovy
@@ -19,10 +19,7 @@ package nz.net.ultraq.thymeleaf.testing
 import org.thymeleaf.testing.templateengine.report.ConsoleTestReporter
 import org.thymeleaf.testing.templateengine.report.ITestReporter
 import org.thymeleaf.testing.templateengine.testable.ITest
-import org.thymeleaf.testing.templateengine.testable.ITestIterator
-import org.thymeleaf.testing.templateengine.testable.ITestParallelizer
 import org.thymeleaf.testing.templateengine.testable.ITestResult
-import org.thymeleaf.testing.templateengine.testable.ITestSequence
 
 /**
  * Provides access to the last test result so it can be interrogated by the test
@@ -37,96 +34,19 @@ class JUnitTestReporter implements ITestReporter {
 	 */
 	ITestResult lastResult
 
-	private final ConsoleTestReporter testReporter = new ConsoleTestReporter()
+	@Delegate
+	private final ITestReporter testReporter
 
-	@Override
-	void iterationEnd(String executionId, int nestingLevel, ITestIterator iterator,
-		int iterationNumber, int okTests, int totalTests, long executionTimeNanos) {
+	/**
+	 * Constructor, configure this custom reporter to use the given underlying
+	 * test reporter as the delegate.  Defaults to a {@link ConsoleTestReporter}
+	 * if not specified.
+	 * 
+	 * @param testReporter
+	 */
+	JUnitTestReporter(ITestReporter testReporter = new ConsoleTestReporter()) {
 
-		testReporter.iterationEnd(executionId, nestingLevel, iterator, iterationNumber,
-				okTests, totalTests, executionTimeNanos)
-	}
-
-	@Override
-	void iterationStart(String executionId, int nestingLevel, ITestIterator iterator,
-		int iterationNumber) {
-
-		testReporter.iterationStart(executionId, nestingLevel, iterator, iterationNumber)
-	}
-
-	@Override
-	void iteratorEnd(String executionId, int nestingLevel, ITestIterator iterator,
-		int okTests, int totalTests, long executionTimeNanos) {
-
-		testReporter.iteratorEnd(executionId, nestingLevel, iterator, okTests, totalTests,
-				executionTimeNanos)
-	}
-
-	@Override
-	void iteratorStart(String executionId, int nestingLevel, ITestIterator iterator) {
-
-		testReporter.iteratorStart(executionId, nestingLevel, iterator)
-	}
-
-	@Override
-	void parallelizerEnd(String executionId, int nestingLevel, ITestParallelizer parallelizer,
-		int okTests, int totalTests, long executionTimeNanos) {
-
-		testReporter.parallelizerEnd(executionId, nestingLevel, parallelizer, okTests, totalTests,
-				executionTimeNanos)
-	}
-
-	@Override
-	void parallelizerStart(String executionId, int nestingLevel, ITestParallelizer parallelizer) {
-
-		testReporter.parallelizerStart(executionId, nestingLevel, parallelizer)
-	}
-
-	@Override
-	void parallelThreadEnd(String executionId, int nestingLevel, ITestParallelizer parallelizer,
-		int threadNumber, int okTests, int totalTests, long executionTimeNanos) {
-
-		testReporter.parallelThreadEnd(executionId, nestingLevel, parallelizer, threadNumber, okTests,
-				totalTests, executionTimeNanos)
-	}
-
-	@Override
-	void parallelThreadStart(String executionId, int nestingLevel, ITestParallelizer parallelizer,
-		int threadNumber) {
-
-		testReporter.parallelThreadStart(executionId, nestingLevel, parallelizer, threadNumber)
-	}
-
-	@Override
-	void sequenceEnd(String executionId, int nestingLevel, ITestSequence sequence,
-		int okTests, int totalTests, long executionTimeNanos) {
-
-		testReporter.sequenceEnd(executionId, nestingLevel, sequence, okTests, totalTests,
-				executionTimeNanos)
-	}
-
-	@Override
-	void sequenceStart(String executionId, int nestingLevel, ITestSequence sequence) {
-
-		testReporter.sequenceStart(executionId, nestingLevel, sequence)
-	}
-
-	@Override
-	void executionStart(String executionId) {
-
-		testReporter.executionStart(executionId)
-	}
-
-	@Override
-	void executionEnd(String executionId, int okTests, int totalTests, long executionTimeNanos) {
-
-		testReporter.executionEnd(executionId, okTests, totalTests, executionTimeNanos)
-	}
-
-	@Override
-	void testStart(String executionId, int nestingLevel, ITest test, String testName) {
-
-		testReporter.testStart(executionId, nestingLevel, test, testName)
+		this.testReporter = testReporter
 	}
 
 	@Override
@@ -134,41 +54,5 @@ class JUnitTestReporter implements ITestReporter {
 
 		lastResult = result
 		testReporter.testEnd(executionId, nestingLevel, test, testName, result, executionTimeNanos)
-	}
-
-	@Override
-	boolean isAllOK() {
-
-		return testReporter.isAllOK()
-	}
-
-	@Override
-	long getTotalExecutionTimeMs() {
-
-		return testReporter.getTotalExecutionTimeMs()
-	}
-
-	@Override
-	Set<String> getAllTestNames() {
-
-		return testReporter.getAllTestNames()
-	}
-
-	@Override
-	ITestResult getResultByTestName(String testName) {
-
-		return testReporter.getResultByTestName(testName)
-	}
-
-	@Override
-	long getExecutionTimeMsByTestName(String testName) {
-
-		return testReporter.getExecutionTimeMsByTestName(testName)
-	}
-
-	@Override
-	void reset() {
-
-		testReporter.reset()
 	}
 }
